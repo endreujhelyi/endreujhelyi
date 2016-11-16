a416e2a93139c74bec00414ba6930f3275fbdff2
Endre Ujhelyi <endreujhelyi@gmail.com>
2016-11-16 15:39:12 +0100

    Wednesday tasks done

---
+++ week-05/day-3/number_1_2_3.py
@@ -0,0 +1,42 @@
+
+# create a function that takes a number and divides ten with it and prints the result
+# it should print "fail" if it is divided by 0
+
+def divider(user_num):
+    try:
+        return (10 / user_num)
+    except ZeroDivisionError:
+        return ('fail')
+
+
+
+# write a function that takes a filename and returns the number of lines the
+# file consists. It should return zero if the file not exists.
+
+def line_counter(file_name):
+    try:
+        f = open(file_name, 'r')
+        length = len(f.readlines())
+        f.close()
+        return length
+    except IOError:
+        return (0)
+
+
+
+# Write a Person class that have a name and a birth_date property
+# It should raise an error of the birthdate is less than 0 or more than 2016
+
+class HorrorError(Exception):
+    pass
+
+class Person:
+
+    def __init__(self, name, birth_date):
+        self.name = name
+        self.birth_date = birth_date
+        self.birth_date_check()
+
+    def birth_date_check(self):
+        if 0 >= self.birth_date or self.birth_date > 2016:
+            raise HorrorError
---
+++ week-05/day-3/test_1_2_3.py
@@ -0,0 +1,18 @@
+import unittest
+from number_1_2_3 import divider, line_counter
+from number_1_2_3 import Person, HorrorError
+
+
+class TddTest(unittest.TestCase):
+    def test_is_ten_divided_by_five_two(self):
+        self.assertEqual(divider(5), 2)
+    def test_is_print_fail_if_input_number_zero(self):
+        self.assertEqual(divider(0), 'fail')
+    def test_is_line_counter_counts_well(self):
+        self.assertEqual(line_counter('yolo.txt'), 11)
+    def test_is_birth_date_check_raises_error_when_4000(self):
+        self.assertRaises(HorrorError, Person, 'Joe', 4007)
+
+
+if __name__ == '__main__':
+    unittest.main()
