c46ea2d7d12950f5356d2378742a0d6c45e9ba07
Endre Ujhelyi <endreujhelyi@gmail.com>
2016-11-18 12:34:18 +0100

    todo main testin

---
+++ week-05/todo/texts.py
@@ -0,0 +1,28 @@
+# Basic dictionary for todo.py
+
+# INTRO STRING
+intro = 'Python Todo application\n=======================\n\nCommand line arguments:\n-l   Lists all the tasks\n-a   Adds a new task\n-r   Removes an task\n-c   Completes an task\n\n'
+
+
+# ========================================
+# ADD TASK
+add = {
+    'no_task': '\nUnable to add: No task is provided\n'
+}
+
+# ARGUMENT ERROR
+arg = {
+    'unsupported': '\nUnsupported argument\n'
+}
+
+# EMPTY LIST
+empty = {
+    'no_todo': '\nNo todos for today!\n'
+}
+
+# TASK
+remove = {
+    'no_index': '\nUnable to remove: No index is provided\n',
+    'out_of_bound': '\nUnable to remove: Index is out of bound\n',
+    'not_number': '\nUnable to remove: Index is not a number\n'
+}
