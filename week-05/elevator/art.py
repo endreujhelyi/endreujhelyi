c63e3bf38615bce636757ec7d7ee426cbf815c68
Endre Ujhelyi <endreujhelyi@gmail.com>
2016-11-21 09:09:28 +0100

    rename elevator folder

---
+++ week-05/elevator/art.py
@@ -0,0 +1,18 @@
+building_parts = {
+    "top": "___________________________________\n`._______________________________.\'",
+    "bottom": ".\'_______________________________`.",
+    "ground_full_1": "  _||_||_[_",
+    "ground_full_2": "_]_||_||_______||_||",
+    "ground_empty": "  _||_||_[___]_||_||_______||_||",
+    "ground_no_lift": "  _||_||_______||_||_______||_||_",
+    "nth_full_1": "   || || [ ",
+    "nth_full_2": " ] || ||       || ||",
+    "nth_empty": "   || || [   ] || ||       || ||",
+    "no_lift": "   || ||       || ||       || ||",
+    "elem": "_"
+}
+
+texts = {
+'intro': '\n\n         ELEVATOR COMMANDS\n|=================================|\n|\n| W - Head to the top!\n| S - Head to the ground!\n| D - Increase population density\n| A - Decrease population density\n|\n| Q - Leave it behind\n|\n|=================================|\n\n',
+'outro': 'That\'s All Folks! See u on the next level\n'
+}
---
+++ week-05/elevator/view_elevator.py
@@ -0,0 +1,43 @@
+
+import art
+import os
+
+class Display:
+
+    def __init__(self):
+        self.art = art.building_parts
+        self.text = art.texts
+
+    def drawing(self, num_floor, position, people):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        #header
+        print ("Floor: " + str(position) + "/12   " + "People: " + str(people) + "/9")
+
+        #draw the building
+        print (self.art['top'])
+        if position == 0:
+            for i in range(num_floor):
+                print (self.art['no_lift'])
+            if people == 0:
+                print (self.art['ground_full_1'] + self.art['elem'] + self.art['ground_full_2'] + str(position))
+            elif people > 0:
+                print (self.art['ground_full_1'] + str(people) + self.art['ground_full_2'] + str(position))
+        elif position > 0:
+            for i in range(num_floor, 0, -1):
+                if i == position:
+                    if people == 0:
+                        print (self.art['nth_empty'] + str(position))
+                    else:
+                        print (self.art['nth_full_1'] + str(people) + self.art['nth_full_2'] + str(position))
+                else:
+                    print (self.art['no_lift'])
+            print (self.art['ground_no_lift'])
+        print (self.art['bottom'])
+
+        #info panel
+        print (self.text['intro'])
+
+    def goodbye(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print (self.text['outro'])
