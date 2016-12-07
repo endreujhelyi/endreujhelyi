0466261cce5c77c3bb0e5e12eeb5803f773df68b
Endre Ujhelyi <endreujhelyi@gmail.com>
2016-12-07 20:30:28 +0100

    string tasks

---
+++ week-07/day-3/strings/float2string.js
@@ -0,0 +1,5 @@
+function float2string(num) {
+  return num.toString();
+}
+
+console.log(float2string(8.29));
---
+++ week-07/day-3/strings/string2float.js
@@ -0,0 +1,5 @@
+function float2string(num) {
+  return parseFloat(num);
+}
+
+console.log(float2string('8.29'));
