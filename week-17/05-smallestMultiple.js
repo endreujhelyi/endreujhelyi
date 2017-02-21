574a8940e995fd6a890e33487685cb1550e69e69
Endre <endreujhelyi@gmail.com>
2017-02-21 12:13:27 +0100

    solution

--- week-17/05-smallestMultiple.js
+++ week-17/05-smallestMultiple.js
@@ -1,15 +1,14 @@
 // What is the smallest positive number that is evenly divisible by
 // all of the numbers from 1 to 20?
 
+let i = 1;
 
-const smallestEvenlyDivisibleFinder = endNumber => {
-  let daNumber = 1;
-  for(let i = endNumber - 1; i > 0; i--) {
-    if(daNumber % i != 0) {
-      daNumber *= i;
-    }
-  }
-  console.log(daNumber);
-};
+while (i % 2 != 0 || i %  3 != 0 || i %  4 != 0 || i %  5 != 0 ||
+  i %  6 != 0 || i %  7 != 0 || i %  8 != 0 || i %  9 != 0 ||
+  i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 ||
+  i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 ||
+  i % 18 != 0 || i % 19 != 0 || i % 20 != 0 ) {
+  i++;
+}
 
-smallestEvenlyDivisibleFinder(20);
+console.log(i);
